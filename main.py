d01783268bb22d3aaaa34e2e5d7d5a8768610e0d
RaihanKabir277 <raihank0198@gmail.com>
2025-08-10 07:24:19 +0600

    basic ui set up add here

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Code will starts here....")
+# print("Code will starts here....")
+
+from tkinter import *
+
+
+# -------UI setup------
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100,100,image=logo_img)
+canvas.pack()
+
+window.mainloop()
